b463aa6786608b7cd210a34ddefe2bd9eacc38f2
CsoOBJ1 <idonthaveemail@email.com>
2023-03-17 17:08:22 +0800

    release v0.1

---
+++ README.md
@@ -0,0 +1,50 @@
+# Introduction
+
+this bot is call `EasyBot`, for the convenience of some troubles when there is no Stickers packs and languages UI to use,
+this is an open source project, if have some problem you can open issue on this repo.
+* *anyone is free to use on your server (if you want.)* *
+
+# Getting started
+
+i'm using yaml for reading content, so you need to install **pyyaml**, for install type the following command:
+
+```shell
+pip3 install pyyaml
+```
+
+Now make sure your computer have install pyTelegrambotAPI, as i'm using pyTelegrambotAPI to code this bot.
+and pyTelegrambotAPI is using python3.
+
+```shell
+pip3 install pyTelegramBotAPI
+```
+
+
+## Usage
+
+1. clone this repo
+2. edit `token.json`, put your bot API on here
+2. Simply run command `python3 main.py` on your server.
+
+Now on your bot type `/start` to start the bot.
+
+## Folder tree
+
+the folder just only 6 files, below is the intro.
+
+```shell
+EasyBot/
+├── keyboard.yaml <- keyboard content
+├── main.py <- main file
+├── readme.md <- read me first please
+├── token.json <- your Telegram API key
+├── LICENSE <- Apache License 2.0 detai
+└── CHANGELOG <- ALL Update will note with this CHANGLOG file. (commits)
+
+
+1 directory, 6 files
+```
+
+# LICENSE
+
+All files in this repository are released under Apache License 2.0. See [LICENSE](LICENSE) file for more information.
---
+++ main.py
@@ -0,0 +1,226 @@
+import telebot
+import logging
+import json
+import datetime
+import yaml
+
+from telebot.types import ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
+from telebot import types
+
+with open('keyboard.yaml', 'r') as f1, open('token.json', 'r') as f2:
+    keyboard = yaml.safe_load(f1)
+    config = json.load(f2)
+
+locals().update(keyboard)
+
+# 取得 bot token
+bot_token = config['token']
+
+# 建立 Telebot 物件
+bot = telebot.TeleBot(bot_token, parse_mode='MarkdownV2')
+
+# 設定 logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+def process_start_keyboard(message, from_back=False):
+    user_first_name = message.from_user.first_name
+    user_last_name = message.from_user.last_name
+
+    if not from_back:
+        welcome_message = keyboard["welcome_message"].format(user_first_name=user_first_name, user_last_name=user_last_name)
+    else:
+        welcome_message = "你已返回主選單，請繼續使用鍵盤提供的文字輸入指令！"
+
+    # 建立鍵盤
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+
+    # 建立鍵盤按鈕
+    stickers_btn = telebot.types.KeyboardButton('Stickers Packs')
+    language_btn = telebot.types.KeyboardButton('Languages')
+    bot_info_btn = telebot.types.KeyboardButton('About Bot')
+    close_keyboard_btn = telebot.types.KeyboardButton('Close Keyboard')
+
+    # 加入按鈕到鍵盤
+    markup.add(stickers_btn, language_btn, bot_info_btn, close_keyboard_btn)
+    bot.send_message(message.chat.id, welcome_message,reply_markup=markup)
+
+def processs_stickers_keyboard(message):
+    user_first_name = message.from_user.first_name
+    user_last_name = message.from_user.last_name
+    replykeyboard_stickers_packs = keyboard["first_keyboard_showing_message_"]["username_asking_stickers_keyboard_selected"].format(user_first_name=user_first_name, user_last_name=user_last_name)
+    # 建立鍵盤
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+
+    # 建立鍵盤按鈕
+    Capoo_Stickerspacks_select_btn = telebot.types.KeyboardButton("Capoo Stickers Packs")
+    HKG_Stickerspacks_select_btn = telebot.types.KeyboardButton("HKG Stickers Packs")
+    Cute_Stickerspacks_selectbtn = telebot.types.KeyboardButton("Cute Stickers Packs")
+    select_cd_back_btn = telebot.types.KeyboardButton("Back")
+    close_keyboard_btn = telebot.types.KeyboardButton("Close Keyboard")
+
+    # 加入按鈕到鍵盤
+    markup.add(Capoo_Stickerspacks_select_btn, HKG_Stickerspacks_select_btn, Cute_Stickerspacks_selectbtn, select_cd_back_btn, close_keyboard_btn)
+    bot.send_message(message.chat.id, replykeyboard_stickers_packs,reply_markup=markup)
+
+def keyboard_start_if_statement(message):
+    if message.chat.type == 'private':
+        if message.text == "Stickers Packs":
+            processs_stickers_keyboard(message)
+            bot.register_next_step_handler(message,keyboard_stickers_if_statement)
+        elif message.text == "Languages":
+            inlinekeyboard_language_install = keyboard["first_keyboard_showing_message_"]["language_install"]
+            bot.reply_to(message,inlinekeyboard_language_install)
+            bot.register_next_step_handler(message,keyboard_start_if_statement)
+        elif message.text == "About Bot":
+            inlinekeyboard_bot_infoo = keyboard["first_keyboard_showing_message_"]["bot_infoo"]
+            bot.reply_to(message,inlinekeyboard_bot_infoo)
+            bot.register_next_step_handler(message,keyboard_start_if_statement)
+        elif message.text == "Close Keyboard":
+            bot.reply_to(message,Keyboard_close,reply_markup=ReplyKeyboardRemove())
+            bot.clear_step_handler_by_chat_id(message.chat.id)
+        else:
+            location = "error_keyboard_text"
+            error_keyboard_text = keyboard['Unknown_text_'][location].format(mess=message.text)
+            bot.reply_to(message, error_keyboard_text)
+            bot.register_next_step_handler(message,keyboard_start_if_statement)
+    else:
+        bot.reply_to(message,private_chat_only)
+        bot.register_next_step_handler(message,keyboard_start_if_statement)
+
+def keyboard_stickers_if_statement(message):
+    if message.chat.type == 'private':
+        if message.text == "Capoo Stickers Packs":
+            capoo_start(message)
+            bot.register_next_step_handler(message,keyboard_stickers_if_statement)
+        elif message.text == "HKG Stickers Packs":
+            HKG_start(message)
+            bot.register_next_step_handler(message,keyboard_stickers_if_statement)
+        elif message.text == "Cute Stickers Packs":
+            Cute_start(message)
+            bot.register_next_step_handler(message,keyboard_stickers_if_statement)
+        elif message.text == "Close Keyboard":
+            bot.reply_to(message,Keyboard_close,reply_markup=ReplyKeyboardRemove())
+            bot.clear_step_handler_by_chat_id(message.chat.id)
+        elif message.text == "Back":
+            process_start_keyboard(message, from_back=True)
+            bot.register_next_step_handler(message,keyboard_start_if_statement)
+        else:
+            location = "stickerspacks_keyboard_UK_TEXT"
+            unknown_text = keyboard['Unknown_text_'][location].format(mess=message.text)
+            bot.reply_to(message, unknown_text)
+            bot.register_next_step_handler(message,keyboard_stickers_if_statement)
+    else:
+        bot.reply_to(message,private_chat_only)
+        bot.register_next_step_handler(message,keyboard_stickers_if_statement)
+
+Capoo_keyb = None
+HKG_keyb = None
+Cute_keyb = None
+
+def capoo_inlinekeyboard():
+    global Capoo_keyb
+    
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    capoo_key1 = types.InlineKeyboardButton("動態 Capoo", callback_data="capoo_dynamic")
+    capoo_key2 = types.InlineKeyboardButton("靜態 Capoo", callback_data="capoo_static")
+    markup.add(capoo_key1, capoo_key2)
+    
+    # 將鍵盤賦值給全局變量 Capoo_keyb
+    Capoo_keyb = markup
+
+def HKG_inlinekeyboard():
+    global HKG_keyb
+    
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    hkg_key1 = types.InlineKeyboardButton("動態 HKG", callback_data="hkg_dynamic")
+    hkg_key2 = types.InlineKeyboardButton("靜態 HKG", callback_data="hkg_static")
+    markup.add(hkg_key1, hkg_key2)
+    
+    # 將鍵盤賦值給全局變量 HKG_keyb
+    HKG_keyb = markup
+
+def Cute_inlinekeyboard():
+    global Cute_keyb
+    
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    cute_key1 = types.InlineKeyboardButton("動態 Cute", callback_data="cute_dynamic")
+    cute_key2 = types.InlineKeyboardButton("靜態 Cute", callback_data="cute_static")
+    markup.add(cute_key1, cute_key2)
+    
+    # 將鍵盤賦值給全局變量 Cute_keyb
+    Cute_keyb = markup
+
+def capoo_start(message):
+    capoo_inlinekeyboard()
+    keyboardcapoo_selected = keyboard["stickerspacks_keyboard_showing_message_selected_"]["select_capoo_stickers_packs"]
+    bot.reply_to(message, keyboardcapoo_selected, reply_markup=Capoo_keyb)
+
+def HKG_start(message):
+    HKG_inlinekeyboard()
+    keyboardhkg_selected = keyboard["stickerspacks_keyboard_showing_message_selected_"]["select_hkg_stickers_packs"]
+    bot.reply_to(message, keyboardhkg_selected, reply_markup=HKG_keyb)
+
+def Cute_start(message):
+    Cute_inlinekeyboard()
+    keyboardcute_selected = keyboard["stickerspacks_keyboard_showing_message_selected_"]["select_cute_stickers_packs"]
+    bot.reply_to(message, keyboardcute_selected, reply_markup=Cute_keyb)
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    global Capoo_keyb
+    
+    if call.message:
+        if call.data == "capoo_dynamic":
+            bot.answer_callback_query(call.id, "即將為你顯示動態 Capoo Stickers。")
+            inlinekeyboard_capoo_message_x = keyboard["inlinekeyboard_showing_message_"]["capoo_inlinekeyboard_selected_dynamic"]
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=inlinekeyboard_capoo_message_x, reply_markup=Capoo_keyb)
+        elif call.data == "capoo_static":
+            bot.answer_callback_query(call.id, "即將為你顯示靜態 Capoo Stickers。")
+            inlinekeyboard_capoo_message_x2 = keyboard["inlinekeyboard_showing_message_"]["capoo_inline_keyboard_selected_static"]
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=inlinekeyboard_capoo_message_x2, reply_markup=Capoo_keyb)
+        elif call.data == "hkg_dynamic":
+            bot.answer_callback_query(call.id, "即將為你顯示動態 HKG Stickers。")
+            inlinekeyboard_hkg_message_x = keyboard["inlinekeyboard_showing_message_"]["hkg_inline_keyboard_selected_dynamic"]
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=inlinekeyboard_hkg_message_x, reply_markup=HKG_keyb)
+        elif call.data == "hkg_static":
+            bot.answer_callback_query(call.id, "即將為你顯示靜態 HKG Stickers。")
+            inlinekeyboard_hkg_message_x2 = keyboard["inlinekeyboard_showing_message_"]["hkg_inline_keyboard_selected_static"]
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=inlinekeyboard_hkg_message_x2, reply_markup=HKG_keyb)
+        elif call.data == "cute_dynamic":
+            bot.answer_callback_query(call.id, "即將為你顯示動態 Cute Stickers。")
+            inlinekeyboard_cute_message_x = keyboard["inlinekeyboard_showing_message_"]["cute_inline_keyboard_selected_dynamic"]
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=inlinekeyboard_cute_message_x, reply_markup=Cute_keyb)
+        elif call.data == "cute_static":
+            bot.answer_callback_query(call.id, "即將為你顯示靜態 Cute Stickers。")
+            inlinekeyboard_cute_message_x2 = keyboard["inlinekeyboard_showing_message_"]["cute_inline_keyboard_selected_static"]
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=inlinekeyboard_cute_message_x2, reply_markup=Cute_keyb)
+
+
+keyboard_opened = False
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    global keyboard_opened
+    if message.chat.type == 'private':
+        process_start_keyboard(message)
+        bot.register_next_step_handler(message,keyboard_start_if_statement)
+    else:
+        bot.reply_to(message,private_chat_only)
+
+@bot.message_handler(content_types=['sticker'])
+def send_stickers(message):
+    if message.chat.type == "private":
+        bot.reply_to(message,"Dont send any Stickers to me.")
+
+# 設定 debug mode handler
+@bot.message_handler(func=lambda message: True)
+def handle_all_messages(message):
+    location = "error_dont_know"
+    unknown_text = keyboard['Unknown_text_'][location].format(mess=message.text)
+    bot.reply_to(message, unknown_text)
+    user_info = f"*{message.from_user.first_name} {message.from_user.last_name}* with username (@{message.from_user.username}) with ID _{message.from_user.id}_"
+    logging.debug(f"Received message from user {user_info} - input: {message.text}")
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
